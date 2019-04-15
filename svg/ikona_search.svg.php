29c547478f8e8866850eecc4cf9548b5150b8b40
Marek Melichar <koprivackej@gmail.com>
2019-04-15 21:07:33 +0200

    pripravujeme

--- svg/ikona_search.svg.php
+++
@@ -1,8 +0,0 @@
-<svg id="ikona_search" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="100%" height="100%" version="1.1" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd"
-viewBox="0 0 694 694"
- xmlns:xlink="http://www.w3.org/1999/xlink">
- <g id="Vrstva_x0020_1">
-  <metadata id="CorelCorpID_0Corel-Layer"/>
-  <path fill="#404D7A" d="M496 437l-31 0 -11 -11c39,-45 62,-104 62,-168 0,-142 -116,-258 -258,-258 -143,0 -258,116 -258,258 0,142 116,258 258,258 64,0 123,-23 168,-62l11 11 0 31 198 198 59 -59 -198 -198zm-238 0c-99,0 -179,-80 -179,-179 0,-99 80,-179 179,-179 99,0 179,80 179,179 0,99 -80,179 -179,179z"/>
- </g>
-</svg>
